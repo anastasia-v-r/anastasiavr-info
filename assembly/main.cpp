de9b3995d519d068e99aa1521c0008c0831c9a14
Dominik Rutter <dominik.rutter@gmail.com>
2019-09-30 06:21:12 -0400

    Better C++ example

--- assembly/main.cpp
+++ assembly/main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
-#include <thread>
-#include <chrono>
 
 int main() {
-    for(int i = 0; i < 1000; i++) {
-        std::cout << "YUhhhhhhhhhhhhhh" << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(20));
-    }
+    int num;
+    std::cout << "Please enter an unsigned integer" << std::endl;
+    std::cin >> num;
+    std::cout << "You enetered [" << num << "]\n";
 }
