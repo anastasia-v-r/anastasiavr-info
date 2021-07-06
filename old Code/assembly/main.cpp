38963953cd9690a7e6456de5d38cb6b1eb06c057
Faunsce <dominik.rutter@gmail.com>
2021-07-06 01:21:42 -0400

    Cloudflare Pages migration

---
+++ old Code/assembly/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdlib.h>
+
+int main() {
+    int num;
+    std::printf("Please enter a number : ");
+    std::scanf("%d", &num);
+    std::printf("\nYou entered the number [%d]\n", num);
+}
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
