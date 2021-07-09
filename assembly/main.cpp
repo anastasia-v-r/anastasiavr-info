ddf52bc31274a11ce18145fb7d79a42018f27140
Faunsce <dominik.rutter@gmail.com>
2021-07-09 00:48:07 -0400

    Remove Old Code

--- assembly/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <stdlib.h>
-
-int main() {
-    int num;
-    std::printf("Please enter a number : ");
-    std::scanf("%d", &num);
-    std::printf("\nYou entered the number [%d]\n", num);
-}
