90bdf9cdf951cc44aac90a9ea1d7b7da44a3742c
Anastasia Valerie Rutter <dominik.rutter@gmail.com>
2023-11-02 01:38:56 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  //publicPath: process.env.NODE_ENV === "production" ? "/faunsce.tk/" : "/",
+  //publicPath: process.env.NODE_ENV === "production" ? "/anastasiavr.info/" : "/",
 };
